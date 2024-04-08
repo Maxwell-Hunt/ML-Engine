8287b5fcd91dec8a8217fdfc564b4df0104d1bbd
Maxwell Hunt <max101hunt@gmail.com>
2024-04-08 18:06:28 -0400

    Setup gitignore & build.bat

---
+++ build.bat
@@ -0,0 +1,2 @@
+g++ -std=c++17 -Wall *.cpp
+if not errorlevel 1 a.exe
---
+++ main.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+int main() {
+    std::cout << "This is going to be a great machine learning project";
+}

8baf8f7b65f72ddd679372382a48095e28ab9a91
Maxwell Hunt <max101hunt@gmail.com>
2024-04-09 11:49:51 -0400

    Added operations with constants

--- operations.cpp
+++ operations.cpp
@@ -12,6 +12,17 @@ float Plus::getPartial(const Expression& x) const {
     return ex1.getPartial(x) + ex2.getPartial(x);
 }
 
+PlusConst::PlusConst(const Expression& ex1, float other) : ex1{ex1}, other{other} {}
+PlusConst::PlusConst(float other, const Expression& ex1) : ex1{ex1}, other{other} {}
+
+float PlusConst::getValue() const {
+    return ex1.getValue() + other;
+}
+
+float PlusConst::getPartial(const Expression& x) const {
+    return ex1.getPartial(x);
+}
+
 Multiply::Multiply(const Expression& ex1, const Expression& ex2) : BinaryOperation(ex1, ex2) {}
 
 float Multiply::getValue() const {
@@ -22,10 +33,37 @@ float Multiply::getPartial(const Expression& x) const {
     return ex1.getValue() * ex2.getPartial(x) + ex2.getValue() * ex1.getPartial(x);
 }
 
+MultiplyConst::MultiplyConst(const Expression& ex1, float other) : ex1{ex1}, other{other} {}
+MultiplyConst::MultiplyConst(float other, const Expression& ex1) : ex1{ex1}, other{other} {}
+
+float MultiplyConst::getValue() const {
+    return ex1.getValue() * other;
+}
+
+float MultiplyConst::getPartial(const Expression& x) const {
+    return ex1.getPartial(x) * other;
+}
+
 Plus operator+(const Expression& a, const Expression& b) {
     return Plus(a, b);
 }
 
+PlusConst operator+(const Expression& a, float other) {
+    return PlusConst(a, other);
+}
+
+PlusConst operator+(float other, const Expression& a) {
+    return PlusConst(a, other);
+}
+
 Multiply operator*(const Expression& a, const Expression& b) {
     return Multiply(a, b);
+}
+
+MultiplyConst operator*(float other, const Expression& a) {
+    return MultiplyConst(a, other);
+}
+
+MultiplyConst operator*(const Expression& a, float other) {
+    return MultiplyConst(a, other);
 }
--- operations.h
+++ operations.h
@@ -20,6 +20,19 @@ public:
     virtual float getPartial(const Expression& x) const override;
 };
 
+class PlusConst : public Expression {
+public:
+    PlusConst(const Expression& ex1, float other);
+    PlusConst(float other, const Expression& ex1);
+    virtual ~PlusConst() override {}
+
+    virtual float getValue() const override;
+    virtual float getPartial(const Expression& x) const override;
+private:
+    const Expression& ex1;
+    float other;
+};
+
 class Multiply : public BinaryOperation {
 public:
     Multiply(const Expression& ex1, const Expression& ex2);
@@ -29,7 +42,26 @@ public:
     virtual float getPartial(const Expression& x) const override;
 };
 
+class MultiplyConst : public Expression {
+public:
+    MultiplyConst(const Expression& ex1, float other);
+    MultiplyConst(float other, const Expression& ex1);
+    virtual ~MultiplyConst() override {}
+
+    virtual float getValue() const override;
+    virtual float getPartial(const Expression& x) const override;
+private:
+    const Expression& ex1;
+    float other;
+};
+
 Plus operator+(const Expression& a, const Expression& b);
+PlusConst operator+(const Expression& a, float other);
+PlusConst operator+(float other, const Expression& a);
+
 Multiply operator*(const Expression& a, const Expression& b);
+MultiplyConst operator*(const Expression& a, float other);
+MultiplyConst operator*(float other, const Expression& a);
+
 
 #endif

0f532271c3bdeb4db149ac608fcfddeea55cc100
Maxwell Hunt <max101hunt@gmail.com>
2024-04-09 12:08:17 -0400

    Made simple linear regression demo

--- main.cpp
+++ main.cpp
@@ -3,12 +3,48 @@
 #include "variable.h"
 
 int main() {
-    Variable v1(5);
-    Variable v2(4);
+    float alpha = 0.05;
+    float sw = 3;
+    float sb = 0;
 
-    Plus p = v1 + v2;
-    Multiply m = v1 * v1;
+    float x1 = 0;
+    float y1 = 1;
+    float x2 = 1;
+    float y2 = 5;
 
-    std::cout << m.getValue() << std::endl;
-    std::cout << m.getPartial(v1) << std::endl;
+    for(std::size_t i = 0;i < 100;i++) {
+        Variable w(sw);
+        Variable b(sb);
+
+        
+
+        auto m = w * x1;
+        auto result1 = m + b;
+
+        auto m2 = w * x2;
+        auto result2 = m2 + b;
+
+        auto neg1 = result1 * -1;
+        auto neg2 = result2 * -1;
+
+        auto error1 = y1 + neg1;
+        auto error2 = y2 + neg2;
+
+        auto se1 = error1 * error1;
+        auto se2 = error2 * error2;
+
+        auto total = se1 + se2;
+        auto mse = total * 0.5;
+
+        float value = mse.getValue();
+        float partialW = mse.getPartial(w);
+        float partialB = mse.getPartial(b);
+
+        std::cout << value << ' ' << sw << ' ' << sb << std::endl;
+
+        // Update w and b
+        sw -= alpha * partialW;
+        sb -= alpha * partialB;
+    }
+    
 }

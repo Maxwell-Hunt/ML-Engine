904dcb9aa98ebd0cec4eb5790d388bbf13002d7b
Maxwell Hunt <max101hunt@gmail.com>
2024-08-26 13:30:49 -0400

    Clean up deprecated files

--- examples/tensor.cpp
+++ examples/tensor.cpp
@@ -8,7 +8,7 @@ std::pair<Tensor<float, 9>, Tensor<float, 9>> generateData() {
     Tensor<float, 9> X;
     Tensor<float, 9> Y;
     for(std::size_t i = 0;i < 9;i++) {
-        X.at({i}) = -5.f  + (float)i * 3.f / 9.f;
+        X.at({i}) = -5.f  + (float)i * 10.f / 9.f;
         Y.at({i}) = X.at({i}) * 2;
     }
 
@@ -29,23 +29,10 @@ int main() {
         Variable mse = reduceAdd(squaredError) / 9.f;
 
         computeGradients(mse);
-        // for(float item : result.partials()) {
-        //     std::cout << item << ' ';
-        // }
-        // std::cout << std::endl;
-        std::cout << "W: " << w.value() << " MSE: " << mse.value() << " Gradient: " << w.partials() << std::endl;
+        std::cout << "W: " << w.value() << " MSE: " << mse.value() << std::endl;
 
         
         float gradient = w.partials();
         w = createVariable(w.value() - alpha * gradient);
     }
 }
-
-// int main() {
-//     Variable x = createMatrixVariable<2, 2>({1, 2, 3, 4});
-//     Matrix<float, 2, 2> y({1, 2, 3, 4});
-
-//     Variable z = matmul(x, y);
-
-//     for(float item : z.value()) std::cout << item << ' ';
-// }
